9009a431768dc4ec6ca69217c70dc9f1137cb032
KatharinaHoff <katharina.hoff@gmail.com>
2020-06-24 08:50:42 +0000

    introduction added

---
+++ lecture/introduction/hello_world.py
@@ -0,0 +1,9 @@
+#!/opt/conda/bin/python3
+
+# The hash serves as a comment character (single line)
+
+""" This is a multi-line
+comment, they are also used as docstrings """
+
+
+print("Hello world")
