e993e239012372ceb8b833d04a016601794a207f
KatharinaHoff <katharina.hoff@gmail.com>
2020-06-24 12:01:37 +0000

    changes when referring to exercises, added missing content to 01_bash

--- lecture/03_programming_essentials/stdin_from_file.py
+++
@@ -1,5 +0,0 @@
-#!/opt/conda/bin/python3
-
-import fileinput
-for line in fileinput.input():
-    print(line)
--- lecture/03_programming_essentials/stdin_on_runtime.py
+++
@@ -1,5 +0,0 @@
-#!/opt/conda/bin/python3
-
-print("Please type something:")
-var = input()
-print("The user entered: " + var)
