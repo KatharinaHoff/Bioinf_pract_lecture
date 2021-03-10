4cd4a6dcdbf9c4c0c28a3ed93a24bc50ef8f5e1d
KatharinaHoff <katharina.hoff@gmail.com>
2021-03-10 11:01:27 +0000

    changes

--- exercises/exercise_2/solution/hello_world.py
+++
@@ -1,9 +0,0 @@
-#!/opt/conda/bin/python3
-
-# The hash serves as a comment character (single line)
-
-''' This is a multi-line
-comment, they are also used as docstrings '''
-
-
-print("Hello world")
